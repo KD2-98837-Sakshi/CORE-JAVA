10aace67512f586d28c69f1874c817dcaefbac51
sakshibhusal25 <sakshibhusal002@gmail.com>
2026-09-15 22:03:22 +0530

    Assignmnt1 updated

--- Assignment1/src/CheckDataType.java
+++ Assignment1/src/CheckDataType.java
@@ -2,22 +2,33 @@ import java.util.Scanner;
 
 public class CheckDataType {
 
-	public static void main(String[] args) {
-		double n1;
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter number: ");
-	
-		if(!sc.hasNextInt()) {
-			if(sc.hasNextDouble()) {
-			n1=sc.nextDouble();
-			System.out.println("number is double");
-			}
-		}
-		else {
-			System.out.println("Number is not double");
-		}
-		
+    public static void main(String[] args) {
 
-	}
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
+
+        if (!sc.hasNextDouble()) {
+            System.out.println("Error: First value is not a double.");
+            return;
+        }
+
+        double n1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+
+        if (!sc.hasNextDouble()) {
+            System.out.println("Error: Second value is not a double.");
+            return;
+        }
+
+        double n2 = sc.nextDouble();
+
+        double average = (n1 + n2) / 2;
+
+        System.out.println("Average = " + average);
+
+        sc.close();
+    }
 }
+
