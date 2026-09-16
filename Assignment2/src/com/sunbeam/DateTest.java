aea95a45945ca4cf3516ab5f2a1f184c3bf32de0
sakshibhusal25 <sakshibhusal002@gmail.com>
2026-09-16 16:27:32 +0530

    Assignment2 updated

--- Assignment2/src/com/sunbeam/DateTest.java
+++ Assignment2/src/com/sunbeam/DateTest.java
@@ -3,7 +3,7 @@ package com.sunbeam;
 public class DateTest {
 
 	public static void main(String[] args) {
-		Date d1=new Date(25, 07, 2004);
+		Date d1 = new Date(05, 07, 2004);
 		d1.displayDate();
 		d1.setDay(01);
 		d1.setMonth(03);
